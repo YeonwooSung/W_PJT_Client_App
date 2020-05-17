31d1323a71dda859ecc9e56aea64e245306b45a8
YeonwooSung <neos960518@gmail.com>
2020-05-17 15:36:12 +0900

    Set up configs for aws amplify

--- w_pjt/index.js
+++ w_pjt/index.js
@@ -3,7 +3,11 @@
  */
 
 import {AppRegistry} from 'react-native';
+import Amplify from 'aws-amplify'
+import config from './aws-exports'
+
 import App from './App';
 import {name as appName} from './app.json';
 
+Amplify.configure(config)
 AppRegistry.registerComponent(appName, () => App);
