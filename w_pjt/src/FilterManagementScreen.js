4667aaccccb293a2b83973985f061e9cdcbaef26
YeonwooSung <neos960518@gmail.com>
2020-05-18 17:49:21 +0900

    Start implement data fetching

--- w_pjt/src/FilterManagementScreen.js
+++ w_pjt/src/FilterManagementScreen.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import {
+    ActivityIndicator,
+    BackHandler,
+    Platform,
     StyleSheet,
     Text,
     View
@@ -13,20 +16,56 @@ export default class FilterManagementScreen extends React.Component {
 
         this.state = {
             isLoaded: false,
+            isRefreshing: false,
+            remain_percent: 0,
         }
+
+        this.receiveData = this.receiveData.bind(this);
+        this.refreshData = this.refreshData.bind(this);
     }
 
     componentDidMount = () => {
-        this.setState({isLoaded: true});
+        this.refreshData();
+        this.focusListener = this.props.navigation.addListener('didFocus', () => {
+            this.refreshData();
+        });
+        //TODO this.blurListener = this.props.navigation.addListener('willBlur', payload => {});
+    }
+
+    refreshData = async () => {
+        this.setState({isRefreshing: true});
+        await this.receiveData();
+    }
+
+    receiveData = async () => {
+        let url = 'https://t6j2dvjrza.execute-api.ap-northeast-2.amazonaws.com/prod/filter';
+
+        try {
+            const response = await fetch(url);
+            const responseJson = await response.json();
+            const filterData = responseJson['filter'];
+            const percentage = filterData['remain_percent'];
+
+            this.setState({
+                isLoaded: true,
+                isRefreshing: false,
+                remain_percent: percentage
+            });
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     render() {
-        let {isLoaded} = this.state;
+        let {isLoaded, isRefreshing, remain_percent} = this.state;
 
         if (isLoaded) {
             return (
                 <View style={styles.container}>
-                    <Text>here</Text>
+                    {isRefreshing &&
+                        <ActivityIndicator size="large" color="red"/>
+                    }
+                    <Text>{remain_percent}</Text>
                 </View>
             );
         }
